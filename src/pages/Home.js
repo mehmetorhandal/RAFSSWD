a1ba152023867ebb11b70b56a7fcd61c06f09e8c
Mehmet Orhan Dal <mehmetorhandal@gmail.com>
2020-10-11 20:04:00 +0300

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import CTA from "../styled/CTA";
+import { Accent, StlyedTitle } from "../styled/Random";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <StlyedTitle>Ready to type?</StlyedTitle>
+      <CTA to="/game">
+        Click or type <Accent>'s'</Accent> to start playing
+      </CTA>
     </div>
   );
 }
