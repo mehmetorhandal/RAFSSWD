c068a1f2ddaef04f6425db1a087aa5a3dd5c8a83
Mehmet Orhan Dal <mehmetorhandal@gmail.com>
2020-10-12 22:27:52 +0300

    added timer component

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,17 +7,44 @@ import {
 } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
-export default function Game() {
+export default function Game({ history }) {
   const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [score]);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMsStr = ("0000" + msPassedStr).slice(-5);
+    //00000 - first 2 are the seconds, last 3 are the ms that have passed
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsStr.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 - parseInt(formattedMsStr.substring(formattedMsStr.length - 3));
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameOver");
+    }
+  }, [seconds, ms, history]);
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-length);
+  };
 
   return (
     <StyledGame>
@@ -26,7 +53,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time:<Strong>02.03</Strong>
+        Time:
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
